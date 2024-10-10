4835bacbae46b7cf1ba45775bc2a9aa21d03f99e
Vitor Ramos <vitor_rolim11@hotmail.com.br>
2024-10-10 18:05:12 -0300

    Criando a classe livro e utilizando jsonpropertyname para converter os arquivos json para os atributos da classe

---
+++ Modelos/Livro.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ClassesAPIs.Modelos;
+
+internal class Livro
+{
+    [JsonPropertyName("titulo")]
+    public string Titulo { get; set; }
+    [JsonPropertyName("autor")]
+    public string Autor { get; set; }
+    [JsonPropertyName("ano_publicacao")]
+    public int AnoPublicacao { get; set; }
+    [JsonPropertyName("genero")]
+    public string Genero { get; set; }
+    [JsonPropertyName("paginas")]
+    public int Paginas { get; set; }
+
+    public string LivroDesc => $"O autor {Autor} fez o livro {Titulo} cujo o gênero é {Genero}, foi publicado em{AnoPublicacao} e tem {Paginas} páginas";
+}
