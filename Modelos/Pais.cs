5f8e5d5379b6f03fd004819ef6fe610e4c9ad5e9
Vitor Ramos <vitor_rolim11@hotmail.com.br>
2024-10-10 18:06:46 -0300

    Criando a classe pais e utilizando jsonpropertyname para converter os arquivos json para os atributos da classe

---
+++ Modelos/Pais.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ClassesAPIs.Modelos;
+
+internal class Pais
+{
+    [JsonPropertyName("nome")]
+    public string NomePais { get; set; }
+    [JsonPropertyName("capital")]
+    public string Capital { get; set; }
+    [JsonPropertyName("populacao")]
+    public int Populacao { get; set; }
+    [JsonPropertyName("continente")]
+    public string Continente { get; set; }
+    [JsonPropertyName("idioma")]
+    public string Idioma { get; set; }
+
+    public string DescricaoPais => $"País:{NomePais}, Capital: {Capital}, População: {Populacao}, Continente: {Continente}, Idioma: {Idioma}";
+}
