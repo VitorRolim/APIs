f52c078aa66cea31eae525d3de29182e594fcd95
Vitor Ramos <vitor_rolim11@hotmail.com.br>
2024-10-10 18:03:10 -0300

    Criando a classe carro e utilizando jsonpropertyname para converter os arquivos json para os atributos da classe

---
+++ Modelos/Carro.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ClassesAPIs.Modelos;
+
+internal class Carro
+{
+    [JsonPropertyName("marca")]
+    public string Marca { get; set; }
+    [JsonPropertyName("modelo")]
+    public string Modelo { get; set; }
+    [JsonPropertyName("ano")]
+    public int Ano { get; set; }
+    public string DescricaoCarro => $"Marca do carro: {Marca}, modelo do carro: {Modelo}, Ano do carro: {Ano}";
+
+}
