26347b5676e1ff4506b24971db2ef53e99413dbb
Vitor Ramos <vitor_rolim11@hotmail.com.br>
2024-10-10 18:05:50 -0300

    Criando a classe filme e utilizando jsonpropertyname para converter os arquivos json para os atributos da classe

---
+++ Modelos/Filmes.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ClassesAPIs.Modelos;
+
+internal class Filmes
+{
+    [JsonPropertyName("title")]
+    public string Titulo { get; set; }
+    [JsonPropertyName("year")]
+    public string Ano { get; set; }
+    [JsonPropertyName("crew")]
+    public string Elenco { get; set; }
+
+    public string FichaTecnica => $"Título: {Titulo}, Ano: {Ano}, Elenco: {Elenco}";
+
+   
+}
